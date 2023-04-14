12b6cf0e6d6eae6ae69e3e97bdecf13a141f206f
smyu24 <67177134+smyu24@users.noreply.github.com>
2023-04-14 15:55:37 -0700

    33

---
+++ mainProj/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="CountyApp",
+    version="1.0",
+    install_requires=[
+        "geemap",
+        "geopandas",
+        "jupyter-server-proxy",
+        "keplergl",
+        "leafmap",
+        "localtileserver",
+        "nbserverproxy",
+        "owslib",
+        "palettable",
+        "plotly",
+        "streamlit",
+        "streamlit-bokeh-events",
+        "streamlit-folium",
+        "streamlit-keplergl",
+        "tropycal",
+        "numpy",
+        "streamlit-on-Hover-tabs"
+    ],
+    # ...
+)
